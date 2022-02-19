5127b26bd290c520bfff58547bdb541a58952393
saritha <saritha.v@yaacreations.com>
2022-02-19 17:33:41 +0530

    vertical counter design

--- src/components/CounterComponent.js
+++ src/components/CounterComponent.js
@@ -1,10 +1,9 @@
 import { useState } from "react";
 
-import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import { IconButton, InputBase } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import InputBase from "@mui/material/InputBase";
 import { makeStyles } from "@mui/styles";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
